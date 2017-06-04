44cadf7ce3b0a7b3d9adccee5ed91cb8ace83fc1
thebigJ007 <jevinsubedi@gmail.com>
2017-06-03 21:34:53 -0500

    #Creating a form to add new albums: #Creating a new album #Create a new view for Albumcreate() #added a url pattern for class Albumcreate() View

--- music/urls.py
+++ music/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     #/music/<album_id>
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 
-    #/music/<album_id>/favorite
+    # /music/album/add
+    url(r'album/add/$', views.AlbumCreate.as_view(), name='album-add'),
 ]
