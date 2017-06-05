b0c00b70ca52bb9508b2e745f680443df9cdc7c0
thebigJ007 <jevinsubedi@gmail.com>
2017-06-05 04:09:30 -0500

    33-Upload Files -changed the database structure of album_logo in class Album() to models.FileField() instead of models.CharField() -added media root directory and media url in project/settings.py -appended urlpatterns to include static and media url -changed album.album_logo to album.album_logo.url in index.html and detail.html to refer to the local url for album logo instead of the typed url since the database structure changed to models.FileField() -added href url to Add an Album button

--- django2/settings.py
+++ django2/settings.py
@@ -120,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') #Actual directory on the computer to store files
+MEDIA_URL ='/media/'    #Relative urls which browsers use to access the files
+
