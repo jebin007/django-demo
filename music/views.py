00b9296489f8ab16e74c8a4370822925b7cc1dfe
thebigJ007 <jevinsubedi@gmail.com>
2017-06-05 04:32:23 -0500

    34:User Registration -added forms.py to override to default django add user parameters -imported User and forms in forms.py -added Class UserForm() -imported render and redirect in views.py -imported authenticate and login in views.py -imported View in views.py -imported UserForm(class we created in forms.py) in views.py

--- music/views.py
+++ music/views.py
@@ -1,6 +1,10 @@
 from django.views import generic
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.core.urlresolvers import reverse_lazy
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login
+from django.views.generic import View
+from .forms import UserForm
 from .models import Album
 
 class IndexView(generic.ListView):
